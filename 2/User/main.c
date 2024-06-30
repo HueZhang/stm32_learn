e0f675dbe83224ca54d4deb28195f93ccee55c75
1234567 <1281762297@qq.com>
2024-06-30 21:35:08 +0800

    开关切换

--- 2/User/main.c
+++ 2/User/main.c
@@ -1,28 +1,33 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
+#include "LED.h"
+#include "Key.h"
+
+
+uint8_t KeyNum;
 
 int main(void)
 {
 	
-	
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-	
+	LED_Init();
+	KEY_Init();
 
 	
 	while(1)
 	{
-		GPIO_ResetBits(GPIOA, GPIO_Pin_0);
-		Delay_ms(500);
+	
 		
+		KeyNum = Key_GetNum();
+		if(KeyNum == 1)
+		{
+			LED1_Turn();
+		}
 		
-		GPIO_SetBits(GPIOA, GPIO_Pin_0);
-		Delay_ms(500);
+		if(KeyNum == 2)
+		{
+			LED3_Turn();
+		}
+		
 		
 	}
 }
